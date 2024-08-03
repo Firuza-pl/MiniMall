21cf717a5e38020ea891c1c0f215efdb01089ced
Firuza <rustemlifiruze@gmail.com>
2024-08-03 23:50:18 +0400

    addingFIles

---
+++ Yasamall/Areas/Admin/Controllers/HallController.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Yasamall.DAL;
+using Yasamall.Models;
+
+namespace Yasamall.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HallController : Controller
+    {
+        private readonly Db_Yasamall _context;
+        private readonly IHostingEnvironment _env;
+
+        public HallController(Db_Yasamall context,
+                              IHostingEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+
+        public IActionResult Index()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            if (!User.IsInRole("Admin"))
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            ViewBag.Active = "Site";
+
+            return View(_context.Halls);
+        }
+
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeleteGet(int? id)
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            if (!User.IsInRole("Admin"))
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            if (id == null) return View("Error");
+
+            Halls hall = await _context.Halls.FindAsync(id);
+
+            if (hall == null) return View("Error");
+
+            ViewBag.Active = "Site";
+
+            return View(hall);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeletePost(int? id)
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            if (!User.IsInRole("Admin"))
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            Halls hall = await _context.Halls.FindAsync(id);
+
+
+            _context.Halls.Remove(hall);
+
+            await _context.SaveChangesAsync();
+            ViewBag.Active = "Site";
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Create()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            if (!User.IsInRole("Admin"))
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            ViewBag.Active = "Home";
+
+            ViewBag.Brands = _context.Brands.Where(b => b.IsFilm == true);
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Halls hall)
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            if (!User.IsInRole("Admin"))
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Active = "Site";
+
+                ViewBag.Brands = _context.Brands;
+                return View(hall);
+            }
+
+
+            Halls newHall = new Halls()
+            {
+                Name = hall.Name,
+                BrandId = hall.BrandId
+            };
+
+            await _context.Halls.AddAsync(newHall);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [ActionName("Edit")]
+        public IActionResult Edit(int? id)
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            if (!User.IsInRole("Admin"))
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            if (id == null) return View("Error");
+
+            Halls hall = _context.Halls.Find(id);
+
+            return View(hall);
+        }
+
+        [ActionName("Edit")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditPost(Halls hall)
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            if (!User.IsInRole("Admin"))
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Xaiş olunur düzgün doldurun.");
+                return View(hall);
+            }
+
+            Halls newHall = await _context.Halls.FindAsync(hall.Id);
+
+            if (newHall == null) return View("Error");
+
+            newHall.Name = hall.Name;
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Yasamall/ViewModel/ProColors.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Yasamall.Models;
+
+namespace Yasamall.ViewModel
+{
+    public class ProColors
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ad mütləq doldurulmalıdır."), StringLength(100, ErrorMessage ="Adın uzunluğu maksimum 100 simvol olmalıdır.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Haqqında qismi mütləq doldurulmalıdır.")]
+        public string Info { get; set; }
+
+        [Required(ErrorMessage ="Qiymət mütləq doldurulmalıdır.")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Hansı brandə aid olduğu mütləq doldurulmalıdır.")]
+        public int BrandsId { get; set; }
+
+        public  IEnumerable<int> ColorsId { get; set; }
+
+        public IEnumerable<int> SizesId { get; set; }
+
+        public virtual ICollection<ProductPhoto> Photos { get; set; }
+
+        [NotMapped]
+        public ICollection<IFormFile> AllPhotos { get; set; }
+    }
+}
